7a467050083a8066bfa05cde12f94015890862d9
shumaikunkun <s1711520@gmail.com>
2019-06-14 05:24:58 +0900

    ルートパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'example/vote'
   post 'example/confirm'
-  root 'example/vote'
+  root 'example#vote'
 end
