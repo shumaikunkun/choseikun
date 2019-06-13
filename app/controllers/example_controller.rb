43a2ebfb92ed3f776590e9984a2a9390d92079cc
shumaikunkun <s1711520@gmail.com>
2019-06-14 05:02:09 +0900

    first heroku

---
+++ app/controllers/example_controller.rb
@@ -0,0 +1,18 @@
+class ExampleController < ApplicationController
+  def vote
+    @day=['7/20','7/21','7/22','7/23','7/24',]
+    @hour=[10,11,12,13,14,15,16]
+  end
+
+  def confirm
+    @day=['7/20','7/21','7/22','7/23','7/24',]
+    @hour=[10,11,12,13,14,15,16]
+
+    @name=params['name']
+    @test=params[:checklist]
+    @test1=Array.new(@day.size){Array.new(@hour.size,0)}
+    params[:checklist].each{|k,v|
+      @test1[@day.index(k.split("-")[0])][@hour.index(k.split("-")[1].to_i)]=v
+    }
+  end
+end
