40754672aabc8cbf30e9f79250363d8f72756e6b
shumaikunkun <s1711520@gmail.com>
2019-06-15 04:34:39 +0900

    時間機能を追加

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,3 +1,4 @@
+
 class ExampleController < ApplicationController
   def vote
     @day=['7/20','7/21','7/22','7/23','7/24',]
@@ -9,6 +10,7 @@ class ExampleController < ApplicationController
     @hour=[10,11,12,13,14,15,16]
 
     @name=params['name']
+    @time=Time.now.strftime("%Y年%m月%d日 %H:%M:%S")
     @test=params[:checklist]
     @test1=Array.new(@day.size){Array.new(@hour.size,0)}
     params[:checklist].each do |k,v|
