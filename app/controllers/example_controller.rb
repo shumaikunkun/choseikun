99d5da13c0ea897bd007eaf956933b7b73eb49b1
shumaikunkun <s1711520@gmail.com>
2019-06-21 21:57:41 +0900

    投票結果一覧可視化

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -13,8 +13,22 @@ class ExampleController < ApplicationController
     @time=Time.now.strftime("%Y年%m月%d日 %H:%M:%S")
     @test=params[:checklist]
     @test1=Array.new(@day.size){Array.new(@hour.size,0)}
-    params[:checklist].each do |k,v|
+    @test.each do |k,v|
+      Shosai.create(name:@name,key:k,value:v)  #データベース格納
       @test1[@day.index(k.split("-")[0])][@hour.index(k.split("-")[1].to_i)]=v
+    end
+  end
+
+  def show
+    @day=['7/20','7/21','7/22','7/23','7/24',]
+    @hour=[10,11,12,13,14,15,16]
+
+    @all_user=Shosai.pluck(:name).uniq
+    @all_datas={}
+    @all_user.each do|u|
+      @all_data={}
+      Shosai.where(name:u).each{|s|@all_data.store(s.key,s.value)}
+      @all_datas.store(u,@all_data)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'example/vote'
   post 'example/confirm'
+  get 'example/show'
   root 'example#vote'
 end
